90e78007987596eb3e33d2d9bbe32a1fb2a572a2
benxta4424 <podean.beni@gmail.com>
2024-07-29 23:19:51 +0100

    Created 3 more methods:pop,contains?,find
    Pop works like this:we have a link that goes behind the next_node and so its gonna have the last value of the linked_list
    When we finish looping,we put that node on nill an so erase the last value
    I also added some edge cases:1.when the list is empty we return nill    2.if the list contains only one item it would be the header and so we put the header to nil reducing the wjole list to nil
    
    Contains is pretty straightforward: we loop through the list and when we find an item we return true else if the loops finishes it means we havent found anithing and we return false
    
    Find works pretty much the same way only we count the positions ourselves
    When we find the item we get the return the current counter,giving us the position of the item

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,5 @@
 class LinkedList
-    class Node
+  class Node
     attr_accessor :value, :next_node
 
     def initialize(_value)
@@ -10,9 +10,9 @@ class LinkedList
 
   def initialize
     @head = nil
-    @size=0
-    @get_head=0
-    @get_tail=0
+    @size = 0
+    @get_head = 0
+    @get_tail = 0
   end
 
   def create_node(value)
@@ -20,76 +20,112 @@ class LinkedList
   end
 
   def append_value(value)
-    current_value=create_node(value)
+    current_value = create_node(value)
     if @head.nil?
-      @head=current_value
-      @get_head=@head
-      @size+=1
+      @head = current_value
+      @get_head = @head
+      @size += 1
     else
-      current_node=@head
-      while current_node.next_node
-        current_node=current_node.next_node
-      end
-      current_node.next_node=current_value
-      @size+=1
+      current_node = @head
+      current_node = current_node.next_node while current_node.next_node
+      current_node.next_node = current_value
+      @size += 1
     end
   end
 
   def prepend_values(value)
-    new_node=create_node(value)
-    #we move the head node down to the next node mimicking a stack behaviour
+    new_node = create_node(value)
+    # we move the head node down to the next node mimicking a stack behaviour
     new_node.next_node = @head
-    @size+=1
-    @head=new_node
+    @size += 1
+    @head = new_node
   end
-
 
   def to_s
-    #if the list is empty we return this message
+    # if the list is empty we return this message
     return "Lista goala!" if @head.nil?
-    elemente=[]
 
-    #we get elements_size to calculate the head
-    #if its 0 we are at the start of the list: we get the head
-    #if its equal to the size of the whole list we are at the end of the list: we get the tail
-    list_size=0
-    current_node=@head
+    elemente = []
+   
+    current_node = @head
     while current_node
-      @get_head=current_node.value if list_size==0
-      elemente<<"(#{current_node.value})"
-      current_node=current_node.next_node
-      list_size+=1
-      if list_size==@size-1
-        @get_tail=current_node.value
-      end
+      elemente << "(#{current_node.value})"
+      current_node = current_node.next_node
     end
-    elemente.join('->')
+    elemente.join("->")
   end
-  
+
   def size
     puts @size
   end
 
   def head
-    puts "The first element is:#{@get_head}"
+    puts "The first element is:#{@get_head=@head.value}"
   end
 
   def tail
+    current_node=@head
+    while current_node
+      @get_tail=current_node.value
+      current_node=current_node.next_node
+      
+    end
+    
     puts "The last element is:#{@get_tail}"
   end
 
   def at(index)
-    current_node=@head
-    list_elements=1
+    current_node = @head
+    list_elements = 1
     while current_node
-      
-      if index==list_elements
+
+      if index == list_elements
         puts "At index #{index} we have the value:#{current_node.value}"
         break
       end
-      
-      current_node=current_node.next_node
-      list_elements+=1
+
+      current_node = current_node.next_node
+      list_elements += 1
     end
+  end
+
+  def pop
+    prev_node = nil
+    current_node = @head
+  
+    return nil if current_node.next_node.nil?
+
+    if current_node.next_node.nil?
+      @head=nil
+      @size-=1
+    end
+
+    while current_node.next_node
+      prev_node=current_node
+      current_node=current_node.next_node
+    end
+    prev_node.next_node=nil
+    @size-=1
+
+  end
+
+  def contains?(value)
+    current_node=@head
+    while current_node
+      return true if current_node.value == value
+      current_node=current_node.next_node
+    end
+    false
+  end
+
+  def find(value)
+    elements_index=1
+    current_node=@head
+    while current_node
+      return elements_index if current_node.value==value
+      current_node=current_node.next_node
+      elements_index+=1
+    end
+    false
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,16 +1,25 @@
 require "./linked_list"
 lista = LinkedList.new
-lista.append_value('karchoui')
+lista.append_value("karchoui")
 lista.append_value(13)
 lista.prepend_values(1)
 lista.prepend_values(2)
 lista.prepend_values(3)
 lista.prepend_values(4)
 
-puts lista.to_s
+
+puts lista
 
 lista.size
 lista.head
 lista.tail
 lista.at(5)
 lista.at(1)
+lista.pop
+item=gets.chomp
+contains=lista.contains?(8)
+puts "Does the list contain #{item} ? #{contains}"
+find_value=gets.chomp
+found=lista.find('karchoui')
+puts "#{find_value} was found at index:#{found}"
+puts lista

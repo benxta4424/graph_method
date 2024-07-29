e6682288279166d06a19fccd8a6899a74c4afa1e
benxta4424 <podean.beni@gmail.com>
2024-07-29 20:58:40 +0100

    Newly added methods:head,tail,at(index)
    The name suggests what these methods do but im going to explain them to be sure
    Head prints the first element in the linked_list
    Tail prints the last element in the linked_list
    At points at the position of index in the linked_list
    I used counting for all 3 methods
    The first element of the count is the head,the last is the tail and index could be whatever the user decides to pick

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -11,6 +11,8 @@ class LinkedList
   def initialize
     @head = nil
     @size=0
+    @get_head=0
+    @get_tail=0
   end
 
   def create_node(value)
@@ -21,13 +23,14 @@ class LinkedList
     current_value=create_node(value)
     if @head.nil?
       @head=current_value
+      @get_head=@head
       @size+=1
     else
-      current_head=@head
-      while current_head.next_node
-        current_head=current_head.next_node
+      current_node=@head
+      while current_node.next_node
+        current_node=current_node.next_node
       end
-      current_head.next_node=current_value
+      current_node.next_node=current_value
       @size+=1
     end
   end
@@ -42,17 +45,51 @@ class LinkedList
 
 
   def to_s
+    #if the list is empty we return this message
     return "Lista goala!" if @head.nil?
     elemente=[]
-    current_head=@head
-    while current_head
-      elemente<<"(#{current_head.value})"
-      current_head=current_head.next_node
+
+    #we get elements_size to calculate the head
+    #if its 0 we are at the start of the list: we get the head
+    #if its equal to the size of the whole list we are at the end of the list: we get the tail
+    list_size=0
+    current_node=@head
+    while current_node
+      @get_head=current_node.value if list_size==0
+      elemente<<"(#{current_node.value})"
+      current_node=current_node.next_node
+      list_size+=1
+      if list_size==@size-1
+        @get_tail=current_node.value
+      end
     end
     elemente.join('->')
   end
   
   def size
     puts @size
+  end
+
+  def head
+    puts "The first element is:#{@get_head}"
+  end
+
+  def tail
+    puts "The last element is:#{@get_tail}"
+  end
+
+  def at(index)
+    current_node=@head
+    list_elements=1
+    while current_node
+      
+      if index==list_elements
+        puts "At index #{index} we have the value:#{current_node.value}"
+        break
+      end
+      
+      current_node=current_node.next_node
+      list_elements+=1
+    end
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,12 +1,16 @@
 require "./linked_list"
 lista = LinkedList.new
 lista.append_value('karchoui')
-lista.append_value('13')
-lista.prepend_values('1')
-lista.prepend_values('2')
-lista.prepend_values('3')
+lista.append_value(13)
+lista.prepend_values(1)
+lista.prepend_values(2)
+lista.prepend_values(3)
 lista.prepend_values(4)
 
 puts lista.to_s
 
 lista.size
+lista.head
+lista.tail
+lista.at(5)
+lista.at(1)
